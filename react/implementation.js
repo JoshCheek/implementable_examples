f29d7344c62add92bfc68053ff06b3384a8eda6e
Josh Cheek <josh.cheek@gmail.com>
2018-03-02 14:18:28 -0600

    Omg, renders a div!!

--- react/implementation.js
+++ react/implementation.js
@@ -1,18 +1,51 @@
 class React {
 }
 
-React.createElement = function() {
+React.createElement = function(klass, props, children) {
+  const dom = {klass, props, children}
+
+  if (typeof klass === 'string') {
+    dom.type = 'concrete'
+  } else {
+    dom.type = 'virtual'
+  }
+
+  return dom
 }
 
 React.Component = class Component {
-  constructor() {
+  constructor(props, context) {
+    this.props = props
   }
 }
 
+function isReactComponent(virtualNode){
+  return React.Component.isPrototypeOf(virtualNode);
+};
 
 
 window.ReactDOM = {
-  render(component, element) {
+  render(virtualNode, parent) {
+    console.log(virtualNode)
+    const {type, klass, props, children} = virtualNode
+    const grandChildren = children // ;)~
+
+    if(type === 'concrete') {
+      const child = document.createElement(klass)
+      if(grandChildren)
+        ReactDOM.render(grandChildren, child)
+      parent.appendChild(child)
+    } else {
+
+      let nextVirtualNode;
+      if(isReactComponent(klass)){
+        nextVirtualNode = new klass(props, {}).render()
+      } else {
+        nextVirtualNode = klass(props);
+      }
+
+      ReactDOM.render(nextVirtualNode, parent)
+    }
   }
 }
 
