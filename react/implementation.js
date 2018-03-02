0366ec10d77d329755bc0fb2fca3bb674215aad5
Josh Cheek <josh.cheek@gmail.com>
2018-03-02 14:46:53 -0600

    Renders strings, arrays, null values, "concrete" and "abstract" components

--- react/implementation.js
+++ react/implementation.js
@@ -1,7 +1,7 @@
 class React {
 }
 
-React.createElement = function(klass, props, children) {
+React.createElement = function(klass, props, ...children) {
   const dom = {klass, props, children}
 
   if (typeof klass === 'string') {
@@ -26,26 +26,48 @@ function isReactComponent(virtualNode){
 
 window.ReactDOM = {
   render(virtualNode, parent) {
-    console.log(virtualNode)
+    if(!virtualNode)
+      return
+
+    if (typeof virtualNode === 'string') {
+      const nextVirtualNode = document.createTextNode(virtualNode)
+      parent.appendChild(nextVirtualNode)
+      return
+    }
+
+    if (Array.isArray(virtualNode)) {
+      virtualNode.forEach(child =>
+        ReactDOM.render(child, parent))
+      return
+    }
+
     const {type, klass, props, children} = virtualNode
     const grandChildren = children // ;)~
+    let nextVirtualNode;
 
     if(type === 'concrete') {
       const child = document.createElement(klass)
-      if(grandChildren)
-        ReactDOM.render(grandChildren, child)
+      if(grandChildren.length !== 0)
+        grandChildren.forEach(grandChild =>
+          ReactDOM.render(grandChild, child))
       parent.appendChild(child)
-    } else {
-
-      let nextVirtualNode;
-      if(isReactComponent(klass)){
-        nextVirtualNode = new klass(props, {}).render()
-      } else {
-        nextVirtualNode = klass(props);
-      }
-
-      ReactDOM.render(nextVirtualNode, parent)
+      return
     }
+
+    if(isReactComponent(klass)) {
+      nextVirtualNode = new klass(props, {}).render()
+      ReactDOM.render(nextVirtualNode, parent)
+      return
+    }
+
+    if (typeof klass === 'function') {
+      nextVirtualNode = klass(props);
+      ReactDOM.render(nextVirtualNode, parent)
+      return
+    }
+
+    console.dir(virtualNode)
+    throw new Error("Shouldn't get here!")
   }
 }
 
